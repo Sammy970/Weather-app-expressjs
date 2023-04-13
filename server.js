9f60a1d4cc71d002d5e004b65863e4c2a8185056
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-04-13 12:49:20 +0530

    Changed PORT for hosting

--- server.js
+++ server.js
@@ -51,6 +51,7 @@ app.post('/submit', (req, res) => {
 
 })
 
-app.listen(3000, function (err) {
-    console.log("Server started at http://localhost:3000");
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(
+    `Server started on PORT ${PORT}`));
