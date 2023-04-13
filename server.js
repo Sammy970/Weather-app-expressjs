fcda2c19e417e005b557cf32a4d309ba3d6cabfd
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-04-13 11:33:42 +0530

    Weather APP using express, open weather api

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "public")))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/submit', (req, res) => {
+    const cityName = req.body.city;
+    // console.log(cityName);
+
+    const api = "37c24afdf34f83fe4191b89391445c00"
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + api + '&units=metric'
+
+    var temp;
+    var pressure;
+    var humidity;
+    var weatherIcon;
+    var desc;
+
+    fetch(url)
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            temp = data["main"].temp;
+            pressure = data["main"].pressure;
+            humidity = data["main"].humidity;
+            weatherIcon = data["weather"][0].icon;
+            desc = data["weather"][0].description;
+
+            const weatherData = {
+                'temp': temp,
+                'pres': pressure,
+                'humidity': humidity,
+                'desc': desc,
+                'icon': weatherIcon
+            }
+
+            res.send(weatherData);
+
+        })
+
+})
+
+app.listen(3000, function (err) {
+    console.log("Server started at http://localhost:3000");
+})
